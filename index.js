e1699fa136d1e9bba50162c85ffb3e2295e8dd43
sarath kumar <sarathkumar232000@gmail.com>
2021-12-31 16:05:40 +0530

    successful using li

--- index.js
+++ index.js
@@ -12,84 +12,77 @@ document.querySelector(".sun").addEventListener("click",function(){
   number++;
 
 });
-const parent = document.querySelector(".parent");
-let child  = document.querySelector(".details");
-const newdiv = document.createElement("div");
-
-newdiv.classList.add("div-0");
-newdiv.classList.add("div-1");
+// const parent = document.querySelector(".parent");
+// let child  = document.querySelector(".details");
+// const newdiv = document.createElement("div");
+const parent = document.querySelector("ul");
+// newdiv.classList.add("div-0");
+// newdiv.classList.add("div-1");
 
 let first = true;
-let numerical = 1;
-let newdivnumber = 2;
-let clearbutton = [];
-let cleatbutton2 = [];
-let divname = [];
+// let numerical = 1;
+// let newdivnumber = 2;
+// let clearbutton = [];
+// let cleatbutton2 = [];
+// let divname = [];
 
 document.querySelector(".typing").addEventListener("keydown",function(event){
 
 if(first === true){
 
     if(event.key === "Enter"){
-
+        document.querySelector("ul").classList.remove("hidden-list");
         var inputvalue = document.querySelector(".typing-input").value;
-        newdiv.innerHTML = "<input>" + "<p>" + inputvalue+ "</p>" ;
-
+        // newdiv.innerHTML = "<input>" + "<p>" + inputvalue+ "</p>" ;
+         document.querySelector("li").innerHTML = inputvalue;
       document.querySelector(".typing-input").value = "";
 
-    parent.insertBefore(newdiv, child);
-    divname.push("div-1");
-   document.querySelector(".div-1 input").setAttribute("type","checkbox");
-   document.querySelector(".div-1 input").addEventListener("click",function(){
-     document.querySelector(".div-1 p").classList.toggle("paraindiv");
-     document.querySelector(".div-1").classList.toggle("remove");
-     if(document.querySelector(".div-1").classList.contains("remove") === true){
-    clearbutton.push("remove");
-  }else {
-    clearbutton.pop();
-  }
-
-   });
+  //   parent.insertBefore(newdiv, child);
+  //   divname.push("div-1");
+  //  document.querySelector(".div-1 input").setAttribute("type","checkbox");
+  //  document.querySelector(".div-1 input").addEventListener("click",function(){
+  //    document.querySelector(".div-1 p").classList.toggle("paraindiv");
+  //    document.querySelector(".div-1").classList.toggle("remove");
+  //    if(document.querySelector(".div-1").classList.contains("remove") === true){
+  //   clearbutton.push("remove");
+  // }else {
+  //   clearbutton.pop();
+  // }
+  //
+  //  });
     first = false;
     }
 
   }else if (event.key === "Enter") {
-    let childnumber = "div-"+numerical;
-    let newdivnumerical = "div-"+newdivnumber;
-     let newdiv2 = document.createElement("div");
-      newdiv2.classList.add("div-0");
-      newdiv2.classList.add(newdivnumerical);
+    // let childnumber = "div-"+numerical;
+    // let newdivnumerical = "div-"+newdivnumber;
+    let newdiv = document.createElement("li");
+     // let newdiv2 = document.createElement("div");
+     //  newdiv2.classList.add("div-0");
+     //  newdiv2.classList.add(newdivnumerical);
       let input2 = document.querySelector(".typing-input");
-      newdiv2.innerHTML = "<input>" + "<p>" + input2.value + "</p>";
-      input2.value = "";
-      parent.insertBefore(newdiv2, document.querySelector("."+ childnumber));
-      divname.push(newdivnumerical);
-      document.querySelector("."+ newdivnumerical+" input").setAttribute("type","checkbox");
-      numerical++;
-      newdivnumber++;
-      document.querySelector("."+newdivnumerical+" input").addEventListener("click",function(){
-        document.querySelector("."+newdivnumerical+" p").classList.toggle("paraindiv");
-        document.querySelector("."+newdivnumerical).classList.toggle("remove");
-        if(document.querySelector("."+newdivnumerical).classList.contains("remove") === true){
-       clearbutton.push("remove");
-       clearbutton2.push(newdivnumerical);
-     }else {
-       clearbutton.pop();
-      let indexvalue = cleatbutton2.indexOf(newdivnumerical);
-       clearbutton2.splice(indexvalue);
-     }
+      // newdiv2.innerHTML = "<input>" + "<p>" + input2.value + "</p>";
 
-      });
+      // parent.insertBefore(newdiv2, document.querySelector("."+ childnumber));
+      parent.insertBefore(newdiv,document.querySelector("li"));
+        newdiv.innerHTML = input2.value;
+        input2.value = "";
+      // document.querySelector("."+ newdivnumerical+" input").setAttribute("type","checkbox");
+      // numerical++;
+      // newdivnumber++;
+      //
+    }
+      // });
 
+//
+//
+//
 
-
-
-  }
 });
-document.querySelector(".clear").addEventListener("click",function(){
-  let cbl = clearbutton.length;
-  for(var i = 0; i < cbl; i++){
-  document.querySelector(".remove").remove();
-
-}
-});
+// document.querySelector(".clear").addEventListener("click",function(){
+//   let cbl = clearbutton.length;
+//   for(var i = 0; i < cbl; i++){
+//   document.querySelector(".remove").remove();
+//
+// }
+// });
